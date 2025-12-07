7f8a986ee281988e4ebfff1a80788ac9fdfead02
Luciano's tech <38385411+PIBSAS@users.noreply.github.com>
2025-12-07 12:13:06 -0300

    Update blocks.js

--- admin/blocks.js
+++ admin/blocks.js
@@ -1,32 +1,43 @@
-function createTitleBlock(text="") {
-const div=document.createElement('div');
-div.className='block';
-div.dataset.type='title';
-div.innerHTML=`<input class="title-input" value="${text}">`;
-return div;
+function createTitleBlock(text = "") {
+  const div = document.createElement("div");
+  div.className = "block";
+  div.dataset.type = "title";
+  div.innerHTML = `<input class="title-input" value="${text}">`;
+  return div;
 }
-function createParagraphBlock(text=""){
-const div=document.createElement('div');
-div.className='block';
-div.dataset.type='paragraph';
-div.innerHTML=`<textarea>${text}</textarea>`;
-return div;
+
+function createParagraphBlock(text = "") {
+  const div = document.createElement("div");
+  div.className = "block";
+  div.dataset.type = "paragraph";
+  div.innerHTML = `<textarea>${text}</textarea>`;
+  return div;
 }
-function parseTexToBlocks(tex){
-const blocks=[];
-const lines=tex.split(/\n\n+/);
-lines.forEach(l=>{
-if(l.startsWith('# ')) blocks.push(createTitleBlock(l.replace('# ','')));
-else blocks.push(createParagraphBlock(l));
-});
-return blocks;
+
+function parseTexToBlocks(tex) {
+  const blocks = [];
+  const lines = tex.split(/\n\n+/);
+
+  lines.forEach(l => {
+    if (l.startsWith("# "))
+      blocks.push(createTitleBlock(l.replace("# ", "")));
+    else
+      blocks.push(createParagraphBlock(l));
+  });
+
+  return blocks;
 }
-function blocksToTex(){
-const ed=document.querySelector('#editor');
-let out="";
-[...ed.children].forEach(b=>{
-if(b.dataset.type==='title') out+=`# ${b.querySelector('input').value}\n\n`;
-else out+=b.querySelector('textarea').value+"\n\n";
-});
-return out.trim();
+
+function blocksToTex() {
+  const ed = document.querySelector("#editor");
+  let out = "";
+
+  [...ed.children].forEach(b => {
+    if (b.dataset.type === "title")
+      out += `# ${b.querySelector("input").value}\n\n`;
+    else
+      out += b.querySelector("textarea").value + "\n\n";
+  });
+
+  return out.trim();
 }
